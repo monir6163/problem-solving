60c657d578925b1cfff306ab4b815aa6d9607e24
monir6163 <monirhossain6163@gmail.com>
2026-04-19 09:08:52 +0600

    solve find largest number

--- main.ts
+++ main.ts
@@ -1,10 +1,22 @@
 //1 Reverse a String
-function reverseString(s:string):string {
-  let result = '';
-  for(let i = s.length - 1; i>=0; i--){
-    result += s[i]
+function reverseString(s: string): string {
+  let result = "";
+  for (let i = s.length - 1; i >= 0; i--) {
+    result += s[i];
   }
   return result;
 }
-const resultString = reverseString("TypeScript");
-console.log(resultString);
+
+//2 Find the Largest Number in an Array
+function findLargestNumber(arr: number[]): number {
+  if (arr.length === 0) {
+    throw new Error("Array cannot be empty");
+  }
+  let largestNum = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > largestNum) {
+      largestNum = arr[i];
+    }
+  }
+  return largestNum;
+}
